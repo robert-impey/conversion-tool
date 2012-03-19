07f66c327d9cad6d259595c4b44ba2477f37f60a
Robert Impey <robert@impey.info>
2012-03-19 21:34:40 +0000

    Converting Fahrenheit to Celsius.

--- ConversionTool/src/conversiontool/ConversionTool.java
+++ ConversionTool/src/conversiontool/ConversionTool.java
@@ -14,6 +14,21 @@ public class ConversionTool {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        if (args.length <= 2) {
+            System.err.println("Please tell me what to do and give me some numbers to convert!");
+        } else {
+            String conversion = args[0];
+            
+            if ("FahrToCel".equals(conversion)) {
+                for (int i = 1; i < args.length; i++) {
+                    double fahr = Double.parseDouble(args[i]);
+                    double cel = ((fahr - 32) / 9) * 5;
+                    
+                    System.out.printf("%f F is %f C\n", fahr, cel);
+                }
+            } else {
+                System.err.println("Unrecognised conversion!");
+            }
+        }
     }
 }
